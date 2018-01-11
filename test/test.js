8574b9210b70db29ea0f7c53884597bf8f600b7b
Ronald Poell <ronaldpoell@gmail.com>
2018-01-11 13:46:22 +0100

    WIP migration to Hapi17

--- test/test.js
+++ test/test.js
@@ -1,15 +1,186 @@
-var Should = require("should");
-var Hapi = require("hapi");
-var Path = require("path");
-var Joi = require("joi");
-var Locale = require("../");
+const Should = require("should");
+const Hapi = require("hapi");
+const Path = require("path");
+const Joi = require("joi");
+const Boom = require("boom");
+const Locale = require("../");
+const Vision = require('vision');
+const Handlebars = require('handlebars');
+
+
+const translateString_en = "All's well that ends well.";
+const translateString_de = "Ende gut, alles gut.";
+const translateString_fr = "Tout est bien qui finit bien.";
+
+var server;
+
+async function setupServer() {
+  const serverOptions = {
+    port: 8047
+  };
+
+  server = new Hapi.Server(serverOptions);
+
+  server.route({
+    method: "GET",
+    path: "/no/language-code/path/parameter",
+    options: {
+      handler: function (request, h) {
+        return (
+          {
+            locale: request.i18n.getLocale(),
+            message: request.i18n.__(translateString_en)
+          }
+        );
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/{languageCode}/localized/resource",
+    options: {
+      handler: function (request, h) {
+        return (
+          {
+            locale: request.i18n.getLocale(),
+            requestedLocale: request.params.languageCode,
+            message: request.i18n.__(translateString_en)
+          }
+        );
+      }
+    }
+  });
+
+  server.route({
+    method: "POST",
+    path: "/{languageCode}/localized/validation",
+    options: {
+      handler: function () {
+      },
+      validate: {
+        payload: {
+          param: Joi.string().required()
+        },
+        failAction: (request, h, error) => {
+          throw Boom.badRequest(request.i18n.__("Validation failed"));
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/localized/with/headers",
+    options: {
+      handler: function (request, h) {
+        return (
+          {
+            locale: request.i18n.getLocale(),
+            requestedLocale: request.headers["language"],
+            message: request.i18n.__(translateString_en)
+          }
+        );
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/localized/with/query",
+    options: {
+      handler: function (request, h) {
+        return (
+          {
+            locale: request.i18n.getLocale(),
+            requestedLocale: request.query["lang"],
+            message: request.i18n.__(translateString_en)
+          }
+        );
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/localized/with/empty",
+    options: {
+      handler: function (request, h) {
+        return (
+          {
+            locale: request.i18n.getLocale(),
+            requestedLocale: request.query["lang"],
+            message: request.i18n.__("")
+          }
+        );
+      }
+    }
+  });
+
+
+  server.route({
+    method: "GET",
+    path: "/{languageCode}/localized/view",
+    options: {
+      handler: function (request, h) {
+        var response = h.view("test",{
+          title: 'Hapi i18n handlebars test',
+          message: "All's well that ends well.",
+          languageCode: request.params.languageCode
+        })
+        //console.log(response);
+        return (response);
+      }
+    }
+  });
+
+}
+
+async function startServer() {
+      try {
+
+        await server.register(Vision);
+
+        server.views({
+          engines: {
+            html: require('handlebars')
+          },
+          relativeTo: __dirname,
+					path: __dirname + '/views',
+        });
+
+
+
+        await server.start();
+
+        console.log('Test Hapi server running at:', server.info.uri);
+
+      }
+      catch (err) {
+        console.log(err);
+        process.exit(1);
+      }
+
+}
+
+before(function(done) {
+  setupServer()
+  .then (() => {
+    startServer()
+  })
+  .then (() => {done()})
+});
+
+after(function() {
+  server.stop({ timeout: 5000 }).then(function (err) {
+    console.log('Test hapi server stopped');
+    process.exit((err) ? 1 : 0);
+  });
+});
 
 describe("Localization", function () {
   describe("Usage of locale in hapi", function () {
 
-    var translateString_en = "All's well that ends well.";
-    var translateString_de = "Ende gut, alles gut.";
-    var translateString_fr = "Tout est bien qui finit bien.";
 
     var doSomething = function (cb) {
       var data = {
@@ -26,119 +197,22 @@ describe("Localization", function () {
       });
     }
 
-    var server = new Hapi.Server();
-    server.connection({port: 8047});
-    server.views({
-      engines: {
-        jade: require("jade")
-      },
-      path: Path.join(__dirname, "views")
-    });
+    it("can be added as plugin", async function () {
 
-    server.route({
-      method: "GET",
-      path: "/no/language-code/path/parameter",
-      handler: function (request, reply) {
-        doSomething(function () {
-          return reply(
-            {
-              locale: request.i18n.getLocale(),
-              message: request.i18n.__(translateString_en)
-            }
-          );
-        });
+      const i18n_options = {
+        locales: ["de", "en", "fr"],
+        directory: __dirname + "/locales",
+        languageHeaderField: "language",
+        queryParameter: "lang",
       }
-    });
-    server.route({
-      method: "GET",
-      path: "/{languageCode}/localized/resource",
-      handler: function (request, reply) {
-        doSomething(function () {
-          return reply(
-            {
-              locale: request.i18n.getLocale(),
-              requestedLocale: request.params.languageCode,
-              message: request.i18n.__(translateString_en)
-            }
-          );
-        });
 
+      try {
+        return server.register({ plugin: {register: Locale.register, pkg: Locale.register.attributes.pkg}, options: i18n_options});
       }
-    });
-    server.route({
-      method: "GET",
-      path: "/{languageCode}/localized/view",
-      handler: function (request, reply) {
-        doSomething(function () {
-          return reply.view("test");
-        });
+      catch (err) {
+        Should.not.exist(err);
+      }
 
-      }
-    });
-    server.route({
-      method: "POST",
-      path: "/{languageCode}/localized/validation",
-      handler: function () {
-      },
-      config: {
-        validate: {
-          payload: {
-            param: Joi.string().required()
-          },
-          failAction: function (request, reply, source, error) {
-            return reply(request.i18n.__("Validation failed")).code(400);
-          }
-        }
-      }
-    });
-    server.route({
-      method: "GET",
-      path: "/localized/with/headers",
-      handler: function (request, reply) {
-        doSomething(function () {
-          return reply(
-            {
-              locale: request.i18n.getLocale(),
-              requestedLocale: request.headers["language"],
-              message: request.i18n.__(translateString_en)
-            }
-          );
-        });
-      }
-    });
-
-    server.route({
-      method: "GET",
-      path: "/localized/with/query",
-      handler: function (request, reply) {
-        doSomething(function () {
-          return reply(
-            {
-              locale: request.i18n.getLocale(),
-              requestedLocale: request.query["lang"],
-              message: request.i18n.__(translateString_en)
-            }
-          );
-        });
-      }
-    });
-
-    it("can be added as plugin", function (done) {
-      server.register(
-        {
-          register: Locale,
-          options: {
-            locales: ["de", "en", "fr"],
-            directory: __dirname + "/locales",
-            languageHeaderField: "language",
-            queryParameter: "lang",
-          }
-        },
-        function (err) {
-          Should.not.exist(err);
-          done();
-        }
-      );
     });
 
     it("extracts the default locale from the configured locales", function () {
@@ -151,140 +225,145 @@ describe("Localization", function () {
       Locale.extractDefaultLocale(["fr", "de"]).should.equal("fr");
     });
 
-    it("uses the default locale if no language code path parameter is available", function (done) {
-      server.inject(
+    it("uses the default locale if no language code path parameter is available", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/no/language-code/path/parameter"
-        },
-        function (response) {
-          response.result.locale.should.equal("de");
-          response.result.message.should.equal(translateString_de);
-          done();
-        }
-      );
+        })
+        .then ( (response) => {
+            response.result.locale.should.equal("de");
+            response.result.message.should.equal(translateString_de);
+          });
     });
 
-    it("uses the requested locale if language code is provided", function (done) {
-      server.inject(
+    it("uses the requested locale if language code is provided", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/fr/localized/resource"
-        },
-        function (response) {
+        })
+        .then( (response) => {
           response.result.locale.should.equal("fr");
           response.result.requestedLocale.should.equal("fr");
           response.result.message.should.equal(translateString_fr);
-          done();
-        }
-      );
+        });
     });
 
-    it("uses the requested locale if language code is provided in headers", function (done) {
-      server.inject(
+    it("uses the requested locale if language code is provided in headers", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/localized/with/headers",
           headers: {
             "language": "fr"
           }
-        },
-        function (response) {
+        })
+        .then ( (response) => {
           response.result.locale.should.equal("fr");
           response.result.requestedLocale.should.equal("fr");
           response.result.message.should.equal(translateString_fr);
-          server.inject(
+          return server.inject(
             {
               method: "GET",
               url: "/localized/with/headers",
               headers: {}
-            },
-            function (response) {
-              response.result.locale.should.equal("de");
-              response.result.message.should.equal(translateString_de);
-              done();
-            }
-          )
-        }
-      )
-    })
+            })
+            .then ( (response) => {
+                response.result.locale.should.equal("de");
+                response.result.message.should.equal(translateString_de);
+              }
+            );
+        });
+    });
 
-    it("uses the language query parameter over the header parameter because this is more explicit", function (done) {
-      server.inject(
+    it("uses the language query parameter over the header parameter because this is more explicit", function () {
+      return server.inject(
         {
           method: "GET",
-          url: "/localized/with/query?lang=fr",
-        },
-        function (response) {
+          url: "/localized/with/query?lang=fr"
+        })
+        .then ( (response) => {
           response.result.locale.should.equal("fr");
           response.result.requestedLocale.should.equal("fr");
           response.result.message.should.equal(translateString_fr);
-          done();
-        }
-      );
+        });
     });
 
-    it("uses the language path parameter over the header parameter because this is more explicit", function (done) {
-      server.inject(
+    it("should return an empty string with empty parameter", function () {
+      return server.inject(
+        {
+          method: "GET",
+          url: "/localized/with/empty",
+          headers: {
+            "language": "fr"
+          }
+        })
+        .then ( (response) => {
+          response.result.locale.should.equal("fr");
+          response.result.message.should.equal("");
+        });
+    });
+
+    it("uses the language path parameter over the header parameter because this is more explicit", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/fr/localized/resource",
           headers: {
             "language": "en"
           }
-        },
-        function (response) {
+        })
+        .then ( (response) => {
           response.result.locale.should.equal("fr");
           response.result.requestedLocale.should.equal("fr");
           response.result.message.should.equal(translateString_fr);
-          done();
-        }
-      );
+        });
     });
 
-    it("translates localized strings in jade templates", function (done) {
-      server.inject(
+    it("translates localized strings in handlebars templates", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/fr/localized/view"
-        },
-        function (response) {
+        })
+        .then ( (response) => {
           response.statusCode.should.equal(200);
-          response.result.should.equal("<!DOCTYPE html><html lang=\"fr\"><body><p>Tout est bien qui finit bien.</p></body></html>");
-          done();
-        }
-      );
+          response.result.should.equal("<!DOCTYPE html><html lang=fr><body><p>Tout est bien qui finit bien.</p></body></html>");
+        });
     });
 
-    it("returns status code NOT-FOUND if the requested locale is not available", function (done) {
-      server.inject(
+    it("returns status code NOT-FOUND if the requested locale is not available", function () {
+      return server.inject(
         {
           method: "GET",
           url: "/en-US/localized/resource"
-        },
-        function (response) {
-          response.result.statusCode.should.equal(404);
-          response.result.message.should.equal("No localization available for en-US");
-          done();
-        }
-      );
+        })
+        .then ( (response) => {
+          const payload = JSON.parse(response.payload);
+          payload.statusCode.should.equal(404);
+          payload.message.should.equal("No localization available for en-US");
+        });
+
     });
 
-    it("is available in the validation failAction handler ", function (done) {
-      server.inject(
+    it("is available in the validation failAction handler ", function () {
+      return server.inject(
         {
           method: "POST",
           url: "/de/localized/validation"
-        },
-        function (response) {
-          response.statusCode.should.equal(400);
-          response.result.should.equal("Prüfung fehlgeschlagen");
-          done();
+        })
+        .then ( (response) => {
+          var payload = JSON.parse(response.payload);
+          payload.statusCode.should.equal(400);
+          payload.message.should.equal("Prüfung fehlgeschlagen");
         }
       );
     });
 
-    it("must asure correct localization when processing requests concurrently", function (done) {
+
+
+    it("must asure correct localization when processing requests concurrently", function () {
       var numIterations = 200;
       var numRequestsPerIteration = 3;
       var numProcessedRequests = 0;
@@ -306,8 +385,8 @@ describe("Localization", function () {
           {
             method: "GET",
             url: "/no/language-code/path/parameter"
-          },
-          function (response) {
+          })
+          .then ( (response) => {
             if (response.result.locale !== "de") {
               ++numErrorsWrongDefaultLocale;
             }
@@ -325,8 +404,8 @@ describe("Localization", function () {
           {
             method: "GET",
             url: "/en/localized/resource"
-          },
-          function (response) {
+          })
+          .then ( (response) => {
             if (response.result.locale !== "en") {
               ++numErrorsWrongRequestedLocale;
             }
@@ -345,8 +424,8 @@ describe("Localization", function () {
           {
             method: "GET",
             url: "/fr/localized/resource"
-          },
-          function (response) {
+          })
+          .then ( (response) => {
             if (response.result.locale !== "fr") {
               ++numErrorsWrongRequestedLocale;
             }
